a6414f10ccf049e86c3e713fef4fff5ea1feb595
Sofia <sofiaxrk@gmail.com>
2022-01-24 20:28:13 +0000

    probando guardar esqueleto de navbar

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -2,11 +2,27 @@ import React from "react";
 
 const Navbar = () => {
 	return (
-		<nav class="container">
-			<h1>Team Evangelion</h1>
+		<>
+		<Nav> 
+			<NavLink>
 
-			<button>hola</button>
-		</nav>
+			<h1>Team Evangelion</h1>
+			</NavLink>
+			<bars/>
+			<NavMenu>
+				<NavLink to="/GitHub" activeStyle>GitHub</NavLink>
+				<NavLink to="/LinkedIn" activeStyle>LinkedIn</NavLink>
+				<NavLink to="/Blog" activeStyle>Blog</NavLink>
+				<NavLink to="/ContactUs" activeStyle>ContactUs</NavLink>
+
+			</NavMenu>
+			<NavBtn>
+				<NavBtnLink to="/signin">
+				Newsletter
+				</NavBtnLink>
+			</NavBtn>
+		</Nav>
+		</>
 	);
 };
 
--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Navbar from "/workspace/react-hello/src/js/component/Navbar.jsx";
 //include images into your bundle
 import Navbar from "/workspace/react-hello/src/js/component/Navbar.jsx";
 
