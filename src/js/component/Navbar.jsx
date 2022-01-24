c9a9dabdf9b1564a4812f9c10e0c45b549cefbc3
Sofia <sofiaxrk@gmail.com>
2022-01-24 16:58:03 +0000

    guardado de rutas actualizadas

---
+++ src/js/component/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+	return (
+		<nav class="container"> 
+			<h1>Team Evangelion</h1>
+
+			<button>hola</button>
+		</nav>
+	);
+};
+
+export default Navbar;
---
+++ src/view/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+//include images into your bundle
+
+//create your first component
+const Home = () => {
+	return <Navbar />;
+};
+
+export default Home;
