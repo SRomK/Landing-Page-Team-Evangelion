167ab577e0582090677744fc914f8edf04677999
Sofia <sofiaxrk@gmail.com>
2022-01-25 14:26:21 +0000

    header terminado

--- src/js/component/Header.jsx
+++ src/js/component/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "../../styles/Header.css";
 
+let imgs = [
+	"https://i0.wp.com/hipertextual.com/wp-content/uploads/2014/05/Evangelion-02.jpg?fit=1200%2C675&ssl=1",
+];
+
 const Header = () => {
 	return (
 		<>
 			<div className="headerBackground">
 				<div className="row">
-					<div className="col-6-md textoClass">
+					<div className="col-4-md textoClass">
 						<h2 className="textoHeader">
 							Lorem ipsum dolor, sit amet consectetur adipisicing
 							elit. Soluta autem fugiat asperiores quibusdam,
@@ -20,10 +24,12 @@ const Header = () => {
 							cumque! Odio quae nemo pariatur repellat!
 						</h2>
 					</div>
-					<div className="col-6-md imgPilotos">
+					<div className="col-4-md"></div>
+					<div className="col-4-md ">
 						<img
-							src="https://i0.wp.com/hipertextual.com/wp-content/uploads/2014/05/Evangelion-02.jpg?fit=1200%2C675&ssl=1"
-							alt=""
+							className="imgPilotos"
+							src={imgs[0]}
+							alt="pilotos evangelion"
 						/>
 					</div>
 				</div>
