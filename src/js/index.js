b2423d0eb090f48abbc6c8156ffd1d13bcfc6bb7
Sofia <sofiaxrk@gmail.com>
2022-01-25 12:15:27 +0000

    saving

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/Index.css";
+import "../styles/Navbar.css";
 
 //import your own components
 import Home from "/workspace/react-hello/src/view/Home.jsx";
