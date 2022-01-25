524ae06f919cd16fd2a6e51f2daa4075210021f0
jseiglie <javierseiglie@gmail.com>
2022-01-25 15:49:28 +0000

    cards update css

--- src/js/view/Home.jsx
+++ src/js/view/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
 				<Card
 					imgSrc="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/220px-React-icon.svg.png"
 					imgAlt="React"
-					texto="React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta (formerly Facebook) and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications. However, React is only concerned with state management and rendering that state to the DOM, so creating React applications usually requires the use of additional libraries for routing, as well as certain client-side functionality."
+					texto="React is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta (formerly Facebook) and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications. However, React is only concerned with state management and rendering that state to the DOM, so creating React applications usually requires the use of additional libraries for routing, as well as certain client-side functionality."
 				/>
 
 				<ContactForm />
