4983a0c0cb35d2578ff036d6d7b6acd75474363f
Sofia <sofiaxrk@gmail.com>
2022-01-26 10:21:00 +0000

    form and footder below div

--- src/js/view/Home.jsx
+++ src/js/view/Home.jsx
@@ -34,10 +34,9 @@ const Home = () => {
 					imgAlt="React"
 					texto="React is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta (formerly Facebook) and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications. However, React is only concerned with state management and rendering that state to the DOM, so creating React applications usually requires the use of additional libraries for routing, as well as certain client-side functionality."
 				/>
-
-				<ContactForm />
-				<FooterPage />
 			</div>
+			<ContactForm />
+			<FooterPage />
 		</div>
 	);
 };
